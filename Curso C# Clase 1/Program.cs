4f1d0d2642819895cf98833ada332436e8a005ae
Alejo de la Cruz <107230234+AlejoDeLaCruz@users.noreply.github.com>
2024-04-17 22:38:57 -0300

    Se sube desafio de sumar numeros

--- Curso C# Clase 1/Program.cs
+++ Curso C# Clase 1/Program.cs
@@ -3,3 +3,18 @@
 string mensaje = "Hola Mundo!!";
 
 Console.WriteLine(mensaje);
+
+int num1;
+int num2;
+
+Console.WriteLine("Ingrese el primer numero a sumar.");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingrese el primer numero a sumar.");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+int total = num1 + num2;
+
+Console.WriteLine("El resultado de la suma es: " + total);
+
+Console.ReadKey();
