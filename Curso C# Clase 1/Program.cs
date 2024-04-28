7f37fc958e8c2738cbf632d5c7f86e55ee8fddd5
Alejo de la Cruz <107230234+AlejoDeLaCruz@users.noreply.github.com>
2024-04-28 00:04:39 -0300

    Cambios varios

--- Curso C# Clase 1/Program.cs
+++ Curso C# Clase 1/Program.cs
@@ -1,20 +1,68 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string mensaje = "Hola Mundo!!";
 
-Console.WriteLine(mensaje);
 
-int num1;
-int num2;
 
-Console.WriteLine("Ingrese el primer numero a sumar.");
-num1 = Convert.ToInt32(Console.ReadLine());
+//SUMA Y RESTA DE VARIABLES:
 
-Console.WriteLine("Ingrese el primer numero a sumar.");
-num2 = Convert.ToInt32(Console.ReadLine());
 
-int total = num1 + num2;
 
-Console.WriteLine("El resultado de la suma es: " + total);
+//string mensaje = "Hola Mundo!!";
 
-Console.ReadKey();
+//Console.WriteLine(mensaje);
+
+//int num1;
+//int num2;
+
+//Console.WriteLine("Ingrese el primer numero a sumar.");
+//num1 = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Ingrese el primer numero a sumar.");
+//num2 = Convert.ToInt32(Console.ReadLine());
+
+//int total = num1 + num2;
+
+//Console.WriteLine("El resultado de la suma es: " + total);
+
+//Console.ReadKey();
+
+
+
+
+
+//BUCLES FOR Y WHILE:
+
+
+
+//int numero;
+//Console.WriteLine("Ingrese un numero");
+//Console.WriteLine("Ingrese 0 para salir");
+
+//numero = int.Parse(Console.ReadLine());
+
+//while (numero != 0)
+//{
+//    if (numero > 0)
+//    {
+//        Console.WriteLine("El numero ingresado es positivo");
+//    }
+//    else if (numero < 0)
+//    {
+//        Console.WriteLine("El numero ingresado es negativo");
+//    }
+//    numero = int.Parse(Console.ReadLine());
+//}
+
+//int numeroTopeParaCalcular; 
+
+//Console.WriteLine("Ingrese el numero maximo para calcular los multiplos de 5:");
+
+//numeroTopeParaCalcular = int.Parse(Console.ReadLine());
+
+//for (int i = 1; i <= numeroTopeParaCalcular; i++)
+//{
+//    if (i % 5 == 0)
+//    {
+//        Console.WriteLine(i + ", "); 
+//    }
+//}
