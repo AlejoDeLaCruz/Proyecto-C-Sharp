4174111b9c80d1e7b1dcf08040e9e01449ca643d
Alejo de la Cruz <107230234+AlejoDeLaCruz@users.noreply.github.com>
2024-05-19 19:11:52 -0300

    Se suben cambios

---
+++ ProyectoWindowsForms/Windows Froms Proyecto c#/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Windows_Froms_Proyecto_c_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            usuario = new TextBox();
+            contraseña = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(256, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(285, 62);
+            label1.TabIndex = 0;
+            label1.Text = "¡Bienvenido!";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(268, 106);
+            label2.Name = "label2";
+            label2.Size = new Size(264, 32);
+            label2.TabIndex = 1;
+            label2.Text = "Registrate para ingresar";
+            label2.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveBorder;
+            button1.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(671, 382);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 45);
+            button1.TabIndex = 2;
+            button1.Text = "Aceptar";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // usuario
+            // 
+            usuario.BackColor = SystemColors.ControlLightLight;
+            usuario.ForeColor = SystemColors.ScrollBar;
+            usuario.Location = new Point(283, 209);
+            usuario.Name = "usuario";
+            usuario.Size = new Size(234, 26);
+            usuario.TabIndex = 3;
+            usuario.TextChanged += textBox1_TextChanged;
+            // 
+            // contraseña
+            // 
+            contraseña.Location = new Point(283, 296);
+            contraseña.Name = "contraseña";
+            contraseña.Size = new Size(234, 26);
+            contraseña.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = SystemColors.ButtonHighlight;
+            label3.Location = new Point(358, 272);
+            label3.Name = "label3";
+            label3.Size = new Size(89, 21);
+            label3.TabIndex = 5;
+            label3.Text = "Contraseña";
+            label3.TextAlign = ContentAlignment.TopCenter;
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.ForeColor = SystemColors.ButtonHighlight;
+            label4.Location = new Point(370, 185);
+            label4.Name = "label4";
+            label4.Size = new Size(64, 21);
+            label4.TabIndex = 6;
+            label4.Text = "Usuario";
+            label4.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(contraseña);
+            Controls.Add(usuario);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private TextBox usuario;
+        private TextBox contraseña;
+        private Label label3;
+        private Label label4;
+    }
+}
